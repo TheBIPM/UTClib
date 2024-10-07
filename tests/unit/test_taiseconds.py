610df442c18ab95d7d8702f7ad26891140fdb3a0
Frédéric Meynadier <Frederic.Meynadier@bipm.org>
2024-10-07 12:54:47 +0000

    Speed test

--- tests/unit/test_taiseconds.py
+++ tests/unit/test_taiseconds.py
@@ -1,5 +1,6 @@
 from utclib import taiseconds
 import numpy as np
+import timeit
 
 
 class TestTaiSeconds:
@@ -7,3 +8,30 @@ class TestTaiSeconds:
         t = taiseconds.taiseconds()
         t.fromMJD(np.array([65000]))
 
+    def test_speed(self):
+        # Use "pytest -s" to see stdout
+        t1 = timeit.timeit(
+            'orig_dt.date(2022, 1, 1)',
+            setup='import datetime as orig_dt',
+            number=1000)
+        t2 = timeit.timeit(
+            'ts.fromUTCCalendar([2022], [1], [1], [0], [0], [0])',
+            setup="from utclib import taiseconds; ts = taiseconds.taiseconds()",
+            number=1000)
+        t3 = timeit.timeit(
+            'ts.fromUTCCalendar(year, month, day, zeros, zeros, zeros)',
+            setup=("from utclib import taiseconds;"
+                   "import numpy as np;"
+                   "ts = taiseconds.taiseconds();"
+                   "year = np.zeros(1000);"
+                   "month = np.zeros(1000);"
+                   "day = np.zeros(1000);"
+                   "zeros = np.zeros(1000);"
+                   "year += 2022;"
+                   "month += 1;"
+                   "day += 1"),
+            number=1)
+        print("\n")
+        print("1000 datetime creation time    : {:.3f} ms".format(t1 * 1000))
+        print("1000 x 1 taiseconds creation time : {:.3f} ms".format(t2 * 1000))
+        print("1 x 1000 taiseconds creation time : {:.3f} ms".format(t3 * 1000))
